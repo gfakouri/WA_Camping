8e0d2095a5b0b59dc6630bdbee9934560b9efd69
gfakouri <garethfakouri832@hotmail.com>
2018-05-16 18:24:23 +0800

    Locations added

--- js/marker.js
+++ js/marker.js
@@ -1,16 +1,123 @@
             // ARRAY OF MARKERS - LOCATION, ICON TYPE, MESSAGE BOX
             var markers = [
                 {
-                coords:{lat:-31.953512, lng:115.857048},
-                iconImage:"/icons/beach_icon2.png",
-                content:'<h2>Lynn MA</h2>'
+                    coords:{lat:-30.297, lng:115.042},
+                    iconImage:"/icons/smallcity.png",
+                    content:'<a href="index.html" class="locations">Jurien Bay</a>'
                 },
                 {
-                coords:{lat:-28.782387, lng:114.607513},
-                iconImage:"/icons/beach_icon2.png",
-                content:'<h2>Amesbury MA</h2>'
+                    coords:{lat:-30.303042, lng:115.037776},
+                    iconImage:"/icons/tents.png",
+                    content:'<a href="index.html" class="locations">Jurien Bay Tourist Park</a>'
                 },
                 {
-                coords:{lat:42.7762,lng:-71.0773}
+                    coords:{lat:-31.35, lng:115.5},
+                    iconImage:"/icons/smallcity.png",
+                    content:'<a href="index.html" class="locations">Guilderton</a>'
+                },
+                {
+                    coords:{lat:-25.9269, lng:113.5339},
+                    iconImage:"/icons/smallcity.png",
+                    content:'<a href="inex.html" class="locations">Denham</a>'
+                },
+                {
+                    coords:{lat:-35.0275, lng:117.884},
+                    iconImage:"/icons/smallcity.png",
+                    content:'<a href="index.html" class="locations">Albany</a>'
+                },
+                {
+                    coords:{lat:-34.2932, lng:115.1392},
+                    iconImage:"/icons/smallcity.png",
+                    content:'<a href="index.html" class="locations">Augusta</a>'
+                },
+                {
+                    coords:{lat:-33.955, lng:115.075},
+                    iconImage:"/icons/smallcity.png",
+                    content:'<a href="index.html" class="locations">Margaret River</a>'
+                },
+                {
+                    coords:{lat:-33.6555, lng:115.35},
+                    iconImage:"/icons/smallcity.png",
+                    content:'<a href="index.html" class="locations">Busselton</a>'
+                },
+                {
+                    coords:{lat:-33.64, lng:115.03},
+                    iconImage:"/icons/beach_icon2.png",
+                    content:'<a href="index.html" class="locations">Yallingup</a>'
+                },
+                {
+                    coords:{lat:-27.7204, lng:114.1625},
+                    iconImage:"/icons/smallcity.png",
+                    content:'<a href="index.html" class="locations">Kalbarri</a>'
+                },
+                {
+                    coords:{lat:-33.8608, lng:121.8896},
+                    iconImage:"/icons/smallcity.png",
+                    content:'<a href="index.html" class="locations">Esperance</a>'
+                },
+                {
+                    coords:{lat:-30.219626, lng:115.037383},
+                    iconImage:"/icons/signpost-2.png",
+                    content:'<a href="index.html" class="locations">Sandy Cape - Junction</a>'
+                },
+                {
+                    coords:{lat:-30.183652, lng:114.998813},
+                    iconImage:"/icons/wildderness_camping.png",
+                    content:'<a href="index.html" class="locations">Sandy Cape - Recreation Park</a>'
+                },
+                {
+                    coords:{lat:-30.182099, lng:114.999151},
+                    iconImage:"/icons/beach_icon2.png",
+                    content:'<a href="index.html" class="locations">Sandy Cape - Beach</a>'
+                },
+                {
+                    coords:{lat:-34.9749, lng:117.3561},
+                    iconImage:"/icons/smallcity.png",
+                    content:'<a href="index.html" class="locations">Denmark</a>'
+                },
+                {
+                    coords:{lat:-34.939875, lng:117.344625},
+                    iconImage:"/icons/cabin-2.png",
+                    content:'<a href="index.html" class="locations">Denmark - Kangaroo Lodge</a>'
+                },
+                {
+                    coords:{lat:-30.069378, lng:114.968373},
+                    iconImage:"/icons/smallcity.png",
+                    content:'<a href="index.html" class="locations">Greenhead</a>'
+                },
+                {
+                    coords:{lat:-30.069959, lng:114.964269},
+                    iconImage:"/icons/beach_icon2.png",
+                    content:'<a href="index.html" class="locations">Greenhead - Dynamite Bay</a>'
+                },
+                {
+                    coords:{lat:-30.069959, lng:114.964269},
+                    iconImage:"/icons/tents.png",
+                    content:'<a href="index.html" class="locations">Greenhead - Caravan Park</a>'
+                },
+                {
+                    coords:{lat:-33.954448, lng:116.131214},
+                    iconImage:"/icons/smallcity.png",
+                    content:'<a href="index.html" class="locations">Bridgetown</a>'
+                },
+                {
+                    coords:{lat:-33.957685, lng:116.137055},
+                    iconImage:"/icons/hotel_0star.png",
+                    content:'<a href="index.html" class="locations">Bridgetown Hotel</a>'
+                },
+                {
+                    coords:{lat:-33.642046, lng:115.034318},
+                    iconImage:"/icons/cave-2.png",
+                    content:'<a href="index.html" class="locations">Ngilgi Cave</a>'
+                },
+                {
+                    coords:{lat:-30.591102, lng:115.160236},
+                    iconImage:"/icons/cromlech.png",
+                    content:'<a href="index.html" class="locations">The Pinnacles Desert</a>'
+                },
+                {
+                    coords:{lat:-34.978048, lng:116.893544},
+                    iconImage:"/icons/tree.png",
+                    content:'<a href="index.html" class="locations">Valley of the Giants Tree Top Walk</a>'
                 }
             ];
